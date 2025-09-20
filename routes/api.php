91a4ed8277715e4022cddc794ab62c95a4f040ce
hirokazutoki <hrkztk1124@gmail.com>
2025-09-20 23:36:30 +0900

    add: authentication

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelpRequestController;
 
@@ -8,6 +9,44 @@ Route::group(['prefix' => 'v1'], function () {
         return response()->json(['version' => 'v1']);
     });
 
-    // TODO: middleware
-    Route::apiResource('help_requests', HelpRequestController::class);
+    Route::post("/external-login", function (Request $request) {
+        // Or some identifier for the user from a third party service
+        $email = $request->input("email");
+        $name = $request->input("name");
+
+        $user = User::firstOrCreate(
+            ["email" => $email],
+            [
+                "name" => $name,
+                "password" => bcrypt(Str::random(16)), // fake password
+            ]
+        );
+
+        $token = $user->createToken("web-client")->plainTextToken;
+
+        return response()->json(["token" => $token]);
+    });
+
+    Route::middleware("auth:sanctum")->group(function () {
+        Route::apiResource('help_requests', HelpRequestController::class);
+
+        Route::get("/bookmarks", function (Request $request) {
+            return $request->user()->bookmarks()->get();
+        });
+
+        Route::post("/bookmarks", function (Request $request) {
+            $request->validate([
+                "title" => "required|string",
+                "url" => "required|url",
+            ]);
+
+            return $request
+                ->user()
+                ->bookmarks()
+                ->create([
+                    "title" => $request->title,
+                    "url" => $request->url,
+                ]);
+        });
+    });
 });
